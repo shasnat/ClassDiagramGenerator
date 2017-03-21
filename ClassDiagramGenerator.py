97dae4dde99dc9862e18855cf3997ec2df1c5666
Shay Hasnat <shayhasnat@gmail.com>
2017-03-21 00:26:40 -0700

    Initial commit. Adding gitignore file and entry point to program.

---
+++ ClassDiagramGenerator.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+class ClassDiagramGenerator(object):
+	"""docstring for ClassDiagramGenerator"""
+	def __init__(self, arg):
+		super(ClassDiagramGenerator, self).__init__()
+		self.arg = arg
